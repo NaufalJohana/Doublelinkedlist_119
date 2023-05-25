deab9535bc7798a6b5ae87379a34cbd918304ba7
NaufalJohana <naufaljohana@gmail.com>
2023-05-25 20:04:28 +0700

    membuat bool doublelinkedlist::search

--- Doublelinkedlist_119/Doublelinkedlist_119.cpp
+++ Doublelinkedlist_119/Doublelinkedlist_119.cpp
@@ -75,3 +75,14 @@ void DoubleLinkedlist::addNode() {
 	current->next = newNode; //step 7
 }
 
+bool DoubleLinkedlist::search(int rollNo, Node** previous, Node** current) {
+	*previous = START; // step 1.a
+	*current = START; //step 1.b
+	while (*current != NULL && rollNo != (*current)->noMhs) { //step 1.c
+		*previous = *current; //step 1.d
+		*current = (*current)->next; // step 1.e
+	}
+	return (*current != NULL);
+}
+
+
