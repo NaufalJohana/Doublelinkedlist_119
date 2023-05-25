24346eba92db319657e0426ad49ea63b8d278cbe
NaufalJohana <naufaljohana@gmail.com>
2023-05-25 15:54:09 +0700

    membuat doublelinked list add node

--- Doublelinkedlist_119/Doublelinkedlist_119.cpp
+++ Doublelinkedlist_119/Doublelinkedlist_119.cpp
@@ -20,4 +20,24 @@ public:
 	bool deleteNode(int rollNo);
 	bool listEmpty();
 	void traverse();
+	void revTraverse();
+	void hapus();
+	void searchData();
 };
+
+DoubleLinkedlist::DoubleLinkedlist() {
+	START = NULL;
+}
+
+void DoubleLinkedlist ::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student : ";
+		cin >> nim;
+		cout << "\nEnter the name of the student: ";
+		cin >> nm;
+		Node* newNode = new Node(); //step 1
+		newNode->noMhs = nim; //step2
+		newNode->name = nim; // step 3
+
+	
